f8fca1370d0eceeeb562928097d8390e3288e7c7
mureT-ani <alphons-tomo0218@docom.ne.jp>
2020-12-02 17:29:33 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(content: params[:content], chacked: false)
+    post = Post.create(content: params[:content], checked: false)
     render json:{ post: post }
   end
 
